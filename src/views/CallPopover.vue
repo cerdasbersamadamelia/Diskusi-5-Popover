426d267743dd05ab884fb811f88200dae2708f34
unknown <pandaidancepat@gmail.com>
2022-11-12 10:23:45 +0700

    first commit

---
+++ src/views/CallPopover.vue
@@ -0,0 +1,27 @@
+<template>
+  <ion-page>   
+    <ion-content class="ion-padding">
+      <ion-button @click="openPopover">Process!</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+  import { IonButton, IonContent, IonPage, popoverController } from '@ionic/vue';
+  import Popover from './HomePage.vue';
+
+  export default{
+    components: { IonButton, IonContent, IonPage },
+    methods: {
+      async openPopover(theEvent){
+        const popover = await popoverController
+        .create({
+          component: Popover,
+          event: theEvent,
+          translucent: true
+        })
+        return popover.present();
+      },
+    },
+  };
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <ion-content class="ion-padding">
+    <ul>
+      <li>NIM: 041643527</li>
+      <li>Nama: Damelia</li>
+      <li>UPBJJ: Jakarta</li>
+    </ul>
+  </ion-content>
+</template>
+
+<script>
+  import { IonContent } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'HomePage',
+    components: { IonContent }
+  });
+</script>
